7eac4c5bde416263db774cfabe3f8cd5e1dadfdf
n0v4to <den.litvinov.94@list.ru>
2023-12-12 22:04:19 +0400

    complite list.h

---
+++ lab2/lab2/List.cpp
@@ -0,0 +1,55 @@
+#include "List.h"
+#include <iostream>
+#include <cstdlib> 
+
+template<typename T>
+void delete_i_element(LinkedList<T>& list, int n) {
+    if (list.empty()) {
+        return;
+    }
+
+    Node<T>* slow = list.get_head();
+    Node<T>* fast = list.get_head();
+
+    for (int i = 0; i < n; i++) {
+        if (!fast) {
+            Node<T>* temp = list.get_head();
+            list.pop_head();
+            delete temp;
+            return;
+        }
+        fast = fast->next;
+    }
+
+    while (fast->next) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    Node<T>* temp = slow->next;
+    slow->next = slow->next->next;
+    delete temp;
+}
+
+
+
+    int main() {
+	    LinkedList<float> list;
+	    list.push_tail(17);
+	    list.push_tail(3);
+	    list.push_tail(10.2);
+	    list.push_tail(1);
+	    list.push_tail(-7);
+	    list.push_tail(-5.33);
+	    list.push_tail(-13);
+	    list.push_tail(4.25);
+	    list.push_tail(5);
+	    list.push_tail(4.5);
+	    cout << "List: ";
+	    list.printList();
+	    int i;
+	    cout << "Index of element (1<i<10)" << endl;
+	    cin >> i;
+        delete_i_element(list, i);
+        list.printList();
+}
--- lab2/lab2/List.h
+++ lab2/lab2/List.h
@@ -1,86 +1,176 @@
 #pragma once
 #include <iostream>
-#include <random>
+#include <stdexcept>
+#include <cstdlib>
+using namespace std;
 
-inline int random(int x, int y) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(x, y);
-    return dist(gen);
-}
+template <typename T>
+struct Node {
+	T data;
+	Node<T>* next;
+	Node(const T& value) : data(value), next(nullptr) {}
+};
 
 template <typename T>
 class LinkedList {
 private:
-    struct Node {
-        int data;
-        Node* next;
-    };
-    Node* head;
-    Node* tail;
-    int size;
-
-    void clear() {
-        while (head != nullptr) {
-            pop_head();
-        }
-    };
+	Node<T>* head;
 
 public:
-    LinkedList() {
-        head = nullptr;
-    }
+	LinkedList() : head(nullptr) {}
 
-    LinkedList(const LinkedList& other) {
+	LinkedList(const LinkedList& other) : head(nullptr) {
+		if (other.head) {
+			head = new Node<T>(other.head->data);
+			Node<T>* current = head;
+			Node<T>* otherCurrent = other.head->next;
 
-    }
+			while (otherCurrent) {
+				current->next = new Node<T>(otherCurrent->data);
+				current = current->next;
+				otherCurrent = otherCurrent->next;
+			}
+		}
+	}
 
-    LinkedList(int size, int minVal, int maxVal) {
+	LinkedList(int size) : head(nullptr) {
+		for (int i = 0; i < size; ++i) {
+			T value = rand() % 100;
+			push_tail(value);
+		}
+	}
 
-    }
+	~LinkedList() {
+		while (head) {
+			Node<T>* temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
 
-    ~LinkedList() {
+	LinkedList& operator=(const LinkedList& other) {
+		if (this != &other) {
+			LinkedList temp(other);
+			std::swap(head, temp.head);
+		}
+		return *this;
+	}
 
-    }
+	void push_tail(const T& value) {
+		Node<T>* newNode = new Node<T>(value);
+		if (!head) {
+			head = newNode;
+		}
+		else {
+			Node<T>* current = head;
+			while (current->next) {
+				current = current->next;
+			}
+			current->next = newNode;
+		}
+	}
 
-    LinkedList& operator=(const LinkedList& other) {
+	void push_tail(const LinkedList& other) {
+		Node<T>* otherCurrent = other.head;
+		while (otherCurrent) {
+			push_tail(otherCurrent->data);
+			otherCurrent = otherCurrent->next;
+		}
+	}
 
-        return *this;
-    }
+	void push_head(const T& value) {
+		Node<T>* newNode = new Node<T>(value);
+		newNode->next = head;
+		head = newNode;
+	}
 
-    void push_tail(int value) {
+	void push_head(const LinkedList& other) {
+		LinkedList temp(other);
+		temp.push_tail(*this);
+		*this = temp;
+	}
 
-    }
+	void pop_head() {
+		if (head) {
+			Node<T>* temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
 
-    void push_tail(const LinkedList& other) {
+	void pop_tail() {
+		if (!head) {
+			return;
+		}
+		if (!head->next) {
+			delete head;
+			head = nullptr;
+			return;
+		}
+		Node<T>* current = head;
+		while (current->next->next) {
+			current = current->next;
+		}
+		delete current->next;
+		current->next = nullptr;
+	}
 
-    }
+	void delete_node(const T& value) {
+		Node<T>* current = head;
+		Node<T>* prev = nullptr;
 
-    void push_head(int value) {
+		while (current) {
+			if (current->data == value) {
+				if (prev) {
+					prev->next = current->next;
+					delete current;
+					current = prev->next;
+				}
+				else {
+					Node<T>* temp = head;
+					head = head->next;
+					delete temp;
+					current = head;
+				}
+			}
+			else {
+				prev = current;
+				current = current->next;
+			}
+		}
+	}
 
-    }
+	const T& operator[](int index) const {
+		if (index < 0 || !head) {
+			throw std::out_of_range("Index out of range or list is empty");
+		}
 
-    void push_head(const LinkedList& other) {
+		Node<T>* current = head;
+		for (int i = 0; i < index; ++i) {
+			if (!current->next) {
+				throw std::out_of_range("Index out of range");
+			}
+			current = current->next;
+		}
+		return current->data;
+	}
 
-    }
+	T& operator[](int index) {
+		return const_cast<T&>(static_cast<const LinkedList&>(*this)[index]);
+	}
+	void printList() const {
+		Node<T>* current = head;
+		while (current) {
+			cout << current->data << " ";
+			current = current->next;
+		}
+		cout << endl;
+	}
+	Node<T>* get_head() {
+		return head;
+	}
+	bool empty() const {
+		return head == nullptr;
+	}
 
-    void pop_head() {
-
-    }
-
-    void pop_tail() {
-
-    }
-
-    void delete_node(int value) {
-
-    }
-
-    int& operator[](int index) {
-
-    }
-
-    const int& operator[](int index) const {
-
-    }
 };

d698d196716225daa4a7a35a4ccd937aba0b76de
n0v4to <den.litvinov.94@list.ru>
2023-12-12 10:09:49 +0400

    first commit

--- lab2/lab2/List.h
+++ lab2/lab2/List.h
@@ -1,1 +1,86 @@
 #pragma once
+#include <iostream>
+#include <random>
+
+inline int random(int x, int y) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(x, y);
+    return dist(gen);
+}
+
+template <typename T>
+class LinkedList {
+private:
+    struct Node {
+        int data;
+        Node* next;
+    };
+    Node* head;
+    Node* tail;
+    int size;
+
+    void clear() {
+        while (head != nullptr) {
+            pop_head();
+        }
+    };
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    LinkedList(const LinkedList& other) {
+
+    }
+
+    LinkedList(int size, int minVal, int maxVal) {
+
+    }
+
+    ~LinkedList() {
+
+    }
+
+    LinkedList& operator=(const LinkedList& other) {
+
+        return *this;
+    }
+
+    void push_tail(int value) {
+
+    }
+
+    void push_tail(const LinkedList& other) {
+
+    }
+
+    void push_head(int value) {
+
+    }
+
+    void push_head(const LinkedList& other) {
+
+    }
+
+    void pop_head() {
+
+    }
+
+    void pop_tail() {
+
+    }
+
+    void delete_node(int value) {
+
+    }
+
+    int& operator[](int index) {
+
+    }
+
+    const int& operator[](int index) const {
+
+    }
+};
